6d054efb714b22f82cb89cb234d5ce34ac58f985
patrickms <github@morris-suzuki.com>
2014-06-21 11:34:40 -0400

    Update safetypass.js

--- safetypass.js
+++ safetypass.js
@@ -1,7 +1,7 @@
 (function() { 
    alert('test');
    var ifrm = document.createElement("IFRAME"); 
-   ifrm.setAttribute("src", "//patrickms.github.io/SafetyPass/safetypass.html"); 
+   ifrm.setAttribute("src", "https://patrickms.github.io/SafetyPass/safetypass.html"); 
    ifrm.style.width = 100+"%"; 
    ifrm.style.height = 200+"px"; 
    document.body.appendChild(ifrm); 
@@ -12,7 +12,7 @@
                                                        && event.origin !== "http://localhost:8000")
        return;
 
-      if(event.data === "Which Domain?")
+     // if(event.data === "Which Domain?")
          ifrm.postMessage(document.domain,"*");
    }
    
