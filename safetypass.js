0efe5184ad128a6aa1f56243136420dc85cecedc
patrickms <github@morris-suzuki.com>
2014-06-21 12:35:33 -0400

    Update safetypass.js

--- safetypass.js
+++ safetypass.js
@@ -4,7 +4,12 @@
    ifrm.setAttribute("src", "https://patrickms.github.io/SafetyPass/safetypass.html"); 
    ifrm.style.width = 100+"%"; 
    ifrm.style.height = 200+"px"; 
+   ifrm.style.top=1+"em";
+   ifrm.style.left=1+"em";
+   ifrm.style.position="fixed";
    document.body.appendChild(ifrm); 
+   document.body.insertBefore(ifrm, document.body.firstChild);
+
 
    function receiveMessage(event)
    {
