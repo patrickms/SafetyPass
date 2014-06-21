f466a831260a504f5383b83ff973f9efde394c9d
patrickms <github@morris-suzuki.com>
2014-06-21 11:01:02 -0400

    Create safetypass.js

---
+++ safetypass.js
@@ -0,0 +1,8 @@
+function makeFrame() { 
+   ifrm = document.createElement("IFRAME"); 
+   ifrm.setAttribute("src", "http://patrickms.github.io/SafetyPass/safetypass.html"); 
+   ifrm.style.width = 100+"%"; 
+   ifrm.style.height = 200+"px"; 
+   document.body.appendChild(ifrm); 
+} 
+
