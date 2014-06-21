1ff76d31c50e5e5c3b22a3b27a3540dc923ff6d2
patrickms <github@morris-suzuki.com>
2014-06-21 11:19:21 -0400

    Update safetypass.js

--- safetypass.js
+++ safetypass.js
@@ -5,4 +5,18 @@
    ifrm.style.width = 100+"%"; 
    ifrm.style.height = 200+"px"; 
    document.body.appendChild(ifrm); 
+
+function receiveMessage(event)
+{
+  if (event.origin !== "http://patrickms.github.io" && event.origin !== "https://patrickms.github.io"
+                                                       && event.origin !== "http://localhost:8000")
+    return;
+
+  alert('received: '+event.data())
+}
+   
+   window.addEventListener("message", receiveMessage, false);
+
+
 })();
+
